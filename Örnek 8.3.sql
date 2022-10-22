29591b85658a506541df62bc581096c0f207e31b
Atalay <92875042+Atalay9@users.noreply.github.com>
2022-10-22 21:36:18 +0300

    ÖDEV 8

---
+++ Örnek 8.3.sql
@@ -0,0 +1,19 @@
+UPDATE employee
+SET name = 'Atalay'
+WHERE id=9;
+
+UPDATE employee
+SET birthday='1999-05-24'
+WHERE name='Vonnie';
+
+UPDATE employee
+SET email = 'XXXXX'
+WHERE email = 'rmongain0@wisc.edu';
+
+UPDATE employee
+SET name = 'Nazım'
+WHERE birthday ='1989-03-26';
+
+UPDATE employee
+SET name = 'Güneş'
+WHERE id=5;

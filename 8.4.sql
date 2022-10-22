f8e528da95fad5273a9d5c81e0b03fc89096791f
Atalay <92875042+Atalay9@users.noreply.github.com>
2022-10-22 21:34:40 +0300

    ÖDEV 8

---
+++ 8.4.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id = 40;
+
+DELETE FROM employee
+WHERE name = 'Reinaldo';
+
+DELETE FROM employee
+WHERE birthday = '1981-05-17';
+
+DELETE FROM employee
+WHERE email = 'mcarabet18@europa.eu';
+
+DELETE FROM employee 
+WHERE id = 44;
